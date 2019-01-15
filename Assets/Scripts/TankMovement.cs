eea86353811d150a96cee643dd4c5ad962a00332
unknown <hathai_milk@hotmail.com>
2019-01-15 22:03:52 +0700

    finger detection

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -10,13 +10,22 @@ public class TankMovement : MonoBehaviour
     public int maxAmmo = 10;
     private int currentAmmo;
 
+    public static Vector3 markerPos;
+
     public Text currentAmmoText;
 
+    public Text markerPosText;
+    public Text tankPosText;
+
     GameObject AmmoPanel;
+    public GameObject marker;
 
     public bool GameMode = false;
 
     private float speed = 10f;
+    private float x_pos = 0f;
+    private float z_pos = 5f;
+    private float y_pos = -13f;
 
     public AudioClip shootSound;
 
@@ -24,6 +33,8 @@ public class TankMovement : MonoBehaviour
     {
         AmmoPanel = GameObject.FindGameObjectWithTag("AmmoPanel");
         currentAmmo = maxAmmo;
+
+        markerPos = new Vector3(0, 0, 0);
     }
 
     void Update()
@@ -35,7 +46,7 @@ public class TankMovement : MonoBehaviour
         }
         else
         {
-            AmmoPanel.SetActive(false);
+            AmmoPanel.SetActive(true);
         }
 
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
@@ -83,6 +94,50 @@ public class TankMovement : MonoBehaviour
             GameMode = !GameMode;
             currentAmmo = maxAmmo;
         }
+
+        ////////////////////////////////////////////////
+        markerPos = marker.transform.position;
+        if((markerPos.x - 100) > 24.3)
+        {
+            x_pos = (float)24.3;
+            //transform.position = new Vector3((float)24.3, transform.position.y, (float)(markerPos.z));
+        }
+        else if((markerPos.x - 100) < -24.3)
+        {
+            x_pos = (float)-24.3;
+            //transform.position = new Vector3((float)-24.3, transform.position.y, (float)(markerPos.z));
+        }
+        else
+        {
+            x_pos = (markerPos.x - 100);
+            //transform.position = new Vector3(markerPos.x - 100, transform.position.y, (float)(markerPos.z));
+        }
+
+        if(markerPos.z > 28)
+        {
+            z_pos = 28f;
+        }
+        else if(markerPos.z < 5)
+        {
+            z_pos = 5f;
+        }
+        else
+        {
+            z_pos = markerPos.z;
+        }
+
+        if(markerPos.y - 13 < -13)
+        {
+            y_pos = (float)-13;
+        }
+        else
+        {
+            y_pos = (float)markerPos.y - 13;
+        }
+
+        transform.position = new Vector3(x_pos, y_pos, z_pos);
+        markerPosText.text = markerPos.ToString();
+        tankPosText.text = transform.position.ToString();
     }
 
     void Shoot()
